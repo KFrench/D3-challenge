4e938cbbab6b0f48ca7a97e9959989c7fbc6769a
Kaleah French <kaleah.french@gmail.com>
2020-04-07 16:28:14 -0400

    scatter complete

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -1,87 +1,147 @@
-var cities = [];
+var states = [];
 var abbreviations = [];
 var poverty = [];
 var obesity = [];
-demoData=[];
-// Load data from csv
-d3.csv("./data.csv").then(function(demoData) {
-    console.log(demoData);
+var stateData = [];
+var xValue = [];
+var yValue = [];
 
-//Log state names
-var citiesdata = demoData.map(data => data.state);
-citiesdata.push(cities);
-console.log("cities", citiesdata);
-
-var abbreviationsdata = demoData.map(data => data.abbr);
-abbreviationsdata.push(abbreviations);
-console.log("abbreviations", abbreviationsdata);
-
-var povertydata = demoData.map(data =>+data.poverty);
-povertydata.push(poverty);
-console.log("poverty", povertydata);
-
-var obesitydata = demoData.map(data => +data.obesity);
-obesitydata.push(obesity);
-console.log("obesity", obesitydata);
-
-
-}).catch(function(error) {
-    console.log(error);
-  });
 //svg container
-var svgHeight = 600;
+var svgHeight = 700;
 var svgWidth = 500;
 
 //margins
 var margin = {
-    top:50,
-    right:50,
+    top:30,
+    right:35,
     bottom:50,
-    left:50
+    left:75
 };
 
 //chart area considering margins
 var chartHeight = svgHeight - margin.top - margin.bottom;
-var chartWidth = svgWidth - margin.right - margin.left;
-
-//create svg container 
-var svg = d3.select("#scatter").append("svg")
-    .attr("height", svgHeight)
-    .attr("width", svgWidth);
-
-//shift according to margins
-var chartGroup = svg.append("g")
-    .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-var yScale = d3.scaleLinear()
-    .domain([0, 20])    
-    .range([chartHeight, 0]);
+var chartWidth = svgWidth - margin.left - margin.right;
 
 var xScale = d3.scaleLinear()
     .domain([0,40])
     .range([0, chartWidth]);
 
+var yScale = d3.scaleLinear()
+    .domain([0, 20])    
+    .range([chartHeight, 0]);
+
+//create svg container 
+var chart = d3.select('#scatter').append('svg')
+    .attr('height', svgHeight)
+    .attr('width', svgWidth)
+    .attr('class', 'chart');
+
+//shift according to margins
+var chartGroup = chart.append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`)
+    .attr('width', chartWidth)
+    .attr('height', chartHeight)
+    .attr('class', 'chartGroup');
+
 //Create axis
-var yAxis = d3.axisLeft(yScale);
 var xAxis = d3.axisBottom(xScale);
 
-//Set x to the bottom and set y axis
-chartGroup.append("g")
-    .attr("transform", `translate(0, ${chartHeight})`)
+var yAxis = d3.axisLeft(yScale);
+
+// Load data from csv
+d3.csv("./data.csv").then(function(demoData) {
+    
+
+    demoData.forEach(function (d) {
+        stateData.push(d);
+        d.poverty = +d.poverty;
+        d.smokes = +d.smokes;
+        d.obesity = +d.obesity;
+        d.age = +d.age;
+        d.income = +d.income;
+        d.healthcare = +d.healthcare;
+        var xValue = d.poverty;
+        var yValue = d.obesity;
+
+    console.log(xValue);
+    console.log(yValue); 
+// Log state names
+    var statedata = demoData.map(data => data.state);
+    statedata.push(states);
+    console.log("state", statedata);
+
+    var abbreviationsdata = demoData.map(data => data.abbr);
+    abbreviationsdata.push(abbreviations);
+    console.log("abbreviations", abbreviationsdata);
+
+
+    var povertydata = demoData.map(data =>+data.poverty);
+    povertydata.push(poverty);
+    console.log("poverty", povertydata);
+
+    var obesitydata = demoData.map(data => +data.obesity);
+    obesitydata.push(obesity);
+    console.log("obesity", obesitydata);
+
+
+chartGroup.append('g')
+    .attr('transform', `translate(0, ${chartHeight})`)
     .call(xAxis);
 
-chartGroup.append("g")
+chartGroup.append('g')
     .call(yAxis);
 
-chartGroup.selectAll(".scatter")
-    .data(demoData)
-    .enter()
-    .append("circle")
-    .classed("scatter", true)
-    .attr("cx", d =>xScale(obesitydata)
-    .attr("cy", d =>yScale(povertydata)
-    .attr("r", 1.5)
-    .style("fill", "#69b3a2")
-        )
+var g = chartGroup.selectAll('circle');
+ console.log(stateData)
 
-        );       
+ // Append circles 
+g
+    .data(stateData)
+    .enter()
+    .append('circle')
+    .attr('cx', d=>xScale(d.obesity))
+    .attr('cy', d=> yScale(d.poverty))
+    .attr('r', 15)
+    .attr('fill', "blue")
+    .style('opacity', '0.3');
+//Add state abbreivations to circles
+
+var circleLabel = chartGroup.selectAll().data(stateData).enter().append('text');
+
+circleLabel.attr("x", function(d) {
+    return xScale(d.obesity);
+  })
+  .attr("y", function(d) {
+    return yScale(d.poverty);
+  })
+  .text(function(d) {
+    return d.abbr;
+  })
+  .attr("font-family", "sans-serif")
+  .attr("font-size", "10px")
+  .attr("text-anchor", "middle")
+  .attr("fill", "white");
+
+chartGroup
+    .append('text')
+    .attr('class', 'title')
+    .attr('y', '-10')
+    .attr('x', '20')
+    .text('Poverty vs Obesity Across U.S. States (2014)');
+    })
+// Create axes labels
+chartGroup.append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("y", 0 - margin.left)
+  .attr("x", 0 - (chartHeight / 2))
+  .attr("dy", "1em")
+  .attr("class", "axisText")
+  .text("Poverty (%)");
+
+chartGroup.append("text")
+  .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + margin.top})`)
+  .attr("class", "axisText")
+  .text("Obese (%)");
+
+
+});
